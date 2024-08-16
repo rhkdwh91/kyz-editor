cd2f723059ae0ec4c5802a3d32d3e2648327047c
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-08-16 20:58:01 +0900

    Update line-height and path for dist imports
    Changed line-height values in styles.css to improve text alignment and readability. Updated import path in example/app/page.tsx to use the dist folder for better consistency. Additionally, incremented the package version to 0.4.1.

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { ChangeEvent, useState } from 'react';
 
-import { Editor, initialState, EditorState } from '../../src';
+import { Editor, initialState, EditorState } from '../../dist';
 
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;

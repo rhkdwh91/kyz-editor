fa71603d5f693c22dfe4613fb44d5b04bdca4754
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-09-20 17:18:09 +0900

    Add user authentication and axios setup to admin app
    Implemented user sign-in and level update mutations using react-query. Added axios instance for both client and server with token refresh logic. Updated configurations, including TypeScript and Next.js settings.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'build/dist/',
     lib: {
-      entry: 'src/index.tsx',
+      entry: 'src/index.ts',
       name: 'kyz-editor',
     },
     rollupOptions: {
